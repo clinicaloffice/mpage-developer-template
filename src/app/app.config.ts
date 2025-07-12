ca2bc4123422ad29d70ef45fd083fe1054333f75
John Simpson <jsimpson@precisionhealth.net>
2025-07-12 10:36:17 -0400

    Initial Commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,47 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  ErrorHandler,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
+import {provideRouter, withHashLocation} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {ConfigService} from '@clinicaloffice/mpage-developer';
+import {ErrorHandlerService} from '@clinicaloffice/mpage-developer';
+
+// Custom date formats
+export const CUSTOM_DATE_FORMATS = {
+  parse: {
+    dateInput: ['dd-MMM-yyyy'],
+  },
+  display: {
+    dateInput: 'dd-MMM-yyyy',
+    dateLabel: 'dd-MMM-yyyy',
+    dateTimeLabel: 'dd-MMM-yyyy HH:mm',
+    locale: 'en-US',
+    monthYearLabel: 'MMM yyyy',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM yyyy',
+  }
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes, withHashLocation()),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync("noop"),
+    provideAppInitializer(() => {
+      // Load the config.json file (if not using, remove the next two lines or the entire provideAppInitializer section.
+      const configService = inject(ConfigService);
+      return configService.loadConfig();
+    }),
+    {provide: ErrorHandler, useClass: ErrorHandlerService}
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {MPageService, MpageLogComponent} from '@clinicaloffice/mpage-developer';
+import {CUSTOM_DATE_FORMATS} from './app.config';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MpageLogComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true
 })
-export class App {
-  protected readonly title = signal('mpage-developer-template');
+export class App implements OnInit {
+  public MPage = inject(MPageService);
+
+  ngOnInit() {
+    this.MPage.setMaxInstances(2, true, 'CHART', false);
+    this.MPage.defaultDateFormats = CUSTOM_DATE_FORMATS;
+  }
 }

f628e4e8eca899e6db4fbcb7bf6a08f5f315a7c2
John Simpson <jsimpson@precisionhealth.net>
2025-10-02 08:35:24 -0400

    Initial Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,28 +1,16 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {CUSTOM_DATE_FORMATS} from './app.config';
 import {
   MPageService,
-  MpageLogComponent,
-  AddressService,
-  AllergyService,
-  CodeValueService,
-  ConfigService,
-  CustomService,
-  DiagnosisService,
-  EncounterService,
-  ErrorHandlerService,
-  OrganizationService, PersonService, PhoneService, ProblemService, ReferenceService
+  MpageLogComponent
 } from '@clinicaloffice/mpage-developer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MpageLogComponent],
+  imports: [MpageLogComponent],
   templateUrl: './app.html',
-  standalone: true,
-  providers: [MPageService, AddressService, AllergyService, CodeValueService, ConfigService, CustomService,
-    DiagnosisService, EncounterService, ErrorHandlerService, OrganizationService, PersonService, PhoneService,
-    ProblemService, ReferenceService]
+  standalone: true
 })
 export class App implements OnInit {
   public MPage = inject(MPageService);
